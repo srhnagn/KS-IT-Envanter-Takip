32edb47f8f93c285a5bcbf41b80bf1a5eae1c1d6
srhnagn <serhanagan68@gmail.com>
2026-08-17 10:42:23 +0300

    style(dashboard): remove yellow / Dashboard text from header

--- ks_it_assets/__manifest__.py
+++ ks_it_assets/__manifest__.py
@@ -14,7 +14,7 @@ Kaleseramik IT Departmanı için özel olarak geliştirilmiş donanım, yazılı
     'author': "Serhan Agan",
     'website': "https://www.kaleseramik.com",
     'category': 'Human Resources',
-    'version': '1.0.9',
+    'version': '1.0.10',
 
     # Modülümüzün KS Envanter altyapısından ve Odoo'nun temel mail (chatter) yapısından faydalanması için
     'depends': ['base', 'mail'],
